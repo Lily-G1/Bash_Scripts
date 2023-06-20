84c89c95eda4747e507cb2bf031179b93030413c
Lilian Anya Galadima <104821662+Lily-G1@users.noreply.github.com>
2023-06-20 10:58:21 +0100

    Create weather.sh

---
+++ weather.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# View weather report in specified location
+
+PROGRAM_NAME=weather
+VERSION=1.0
+YEAR=2022
+DEVELOPER=Lilian
+
+case $1 in
+    -h | --help)
+        echo "$PROGRAM_NAME $VERSION"
+        echo "Copyright $YEAR $DEVELOPER. All rights reserved."
+        echo
+        echo "Usage: weather [options]"
+        echo "Option    Long Option     Description"
+        echo "-h        --help          Display this help message"
+        echo "-l        --location      Display the current weather for given location"
+        ;;
+        -l | --location)
+        curl https://wttr.in/$2
+        ;;
+        *)
+esac
